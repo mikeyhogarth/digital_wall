a028b3f7924610b21fa321d07f03ea48922a91c0
mikeyhogarth <mikehogarth20@hotmail.com>
2012-10-30 12:22:11 +0000

    Allow client select from user management

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -9,11 +9,27 @@ class ConceptsController < ApplicationController
     @client = @presentation.client
   end
   
+  def edit
+    @concept = Concept.find(params[:id])
+    @presentation = @concept.presentation
+  end
+  
+  def update
+    
+    @concept = Concept.find(params[:id])
+    @presentation = @concept.presentation
+        
+    if @concept.update_attributes(params[:concept])    
+      redirect_to presentation_concept_url(:id => @concept.id, :presentation_id => @concept.presentation_id, :notice => 'Concept was successfully updated.')
+    end
+
+  end
   
   def new
     @presentation = Presentation.find(params[:presentation_id])
     @concept = @presentation.concepts.build
-
+    @is_remote = true
+    
     render partial: "form"
   end  
   
--- app/models/concept.rb
+++ app/models/concept.rb
@@ -4,7 +4,6 @@ class Concept < ActiveRecord::Base
   #properties
   attr_accessible :name, :description, :image  
   has_attached_file :image, :styles => { :thumbnail => "200x200", :average => "300x300" }
-
   
   #validation
   validates_presence_of :name, :description
